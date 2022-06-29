825602a16782eb8b767048fc921961c0dd7f6cbd
Apimook Tratree <57552159+apkmew@users.noreply.github.com>
2022-06-29 20:23:48 +0700

    hw

--- CPE KU Year 2/ADT/HW01/03-BMI.c
+++ CPE KU Year 2/ADT/HW01/03-BMI.c
@@ -5,7 +5,7 @@ int main(){
     h /= 100;
     bmi = w / ( h * h );
     printf("BMI: %.4lf and you are ",bmi);
-    if(bmi >= 30)           printf("obuse");
+    if(bmi >= 30)           printf("obese");
     else if(bmi >= 25)      printf("overweight");
     else if(bmi >= 18.6)    printf("healthy");
     else                    printf("underweight");
