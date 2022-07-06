648bfd0265d1572618498e1955cd45571882f633
Apimook Tratree <57552159+apkmew@users.noreply.github.com>
2022-07-06 22:56:13 +0700

    hw02

--- CPE KU Year 2/ADT/HW02/01_Integrate.c
+++ CPE KU Year 2/ADT/HW02/01_Integrate.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    double a,b,A,B,n,x,i,y1,y2,ans=0;
-    scanf("%lf %lf %lf %lf %lf",&a,&b,&A,&B,&n);
+    double a,b,A,B,x,y1,y2,ans=0;
+    int n,i;
+    scanf("%lf %lf %lf %lf %d",&a,&b,&A,&B,&n);
     x = (b-a)/n;
-    for(i=a;i<b;i+=x){
-        y1 = A * sin((M_PI*i)/B);
-        y2 = A * sin((M_PI*(i+x))/B);
+    for(i=0;i<n;i++){
+        y1 = A * sin((M_PI*(a+i*x))/B);
+        y2 = A * sin((M_PI*(a+(i+1)*x))/B);
         ans += ((y1 + y2) * x)/2;
     }
     printf("%.5lf",ans);
