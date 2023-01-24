7b82f0c5cc628df7b9814ea3bf512cd232a0d544
Apimook Tratree <57552159+apkmew@users.noreply.github.com>
2023-01-24 09:05:39 +0700

    probsolve

--- CPE KU Year 2/Problem Solving/Week04/13_twoteams.cpp
+++ CPE KU Year 2/Problem Solving/Week04/13_twoteams.cpp
@@ -5,20 +5,29 @@
     School	: Kasetsart University
     Created	: 23 January 2023 [15:17]
     Algo	: Bipartite
-    Status	: Incomplete
+    Status	: Completed
 */
 #include<bits/stdc++.h>
 using namespace std;
 vector<int> g[100100];
-int mark[100100];
+int mark[100100],ch[100100],cou=0;
+void dfs(int i){
+    ch[i] = cou;
+    mark[i] = 3 - mark[i];
+    for(auto x:g[i]){
+        if(ch[x]!=cou){
+            dfs(x);
+        }
+    }
+}
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0),cout.tie(0);
     int n,m,u,v,i;
     cin >> n >> m;
     for(i=0;i<m;i++){
         cin >> u >> v;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        //g[u].push_back(v);
+        //g[v].push_back(u);
         if(mark[u]==0 && mark[v]==0){
             mark[u] = 1;
             mark[v] = 2;
@@ -30,9 +39,28 @@ int main(){
             mark[v] = 3-mark[u];
         }
         else{
-            g[v].pop_back();
-            
+            if(mark[u]==mark[v]){
+                cou++;
+                dfs(v);
+                if(mark[u]==mark[v]){
+                    cout << i << "\n";
+                    return 0;
+                }
+            }
         }
+        g[u].push_back(v);
+        g[v].push_back(u);
     }
+    cout << i << "\n";
     return 0;
 }
+/*
+5 7
+1 2
+2 3
+3 4
+4 1
+4 5
+5 2
+5 1
+*/
